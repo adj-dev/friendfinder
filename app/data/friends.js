664d684aab8134e4b5a45def53683ad49a604c9e
adj-dev <andrew.johnson2073@gmail.com>
2019-05-31 17:58:22 -0500

    Added more objects to friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
     name: 'Amanda',
-    image: 'https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn',
+    image: 'https://picsum.photos/200/300',
     scores: [
       1,
       3,
@@ -14,5 +14,85 @@ module.exports = [
       1,
       5
     ]
-  }
+  },
+  {
+    name: 'Bill',
+    image: 'https://picsum.photos/200/300',
+    scores: [
+      5,
+      5,
+      2,
+      1,
+      5,
+      4,
+      2,
+      1,
+      3,
+      5
+    ]
+  },
+  {
+    name: 'Frank',
+    image: 'https://picsum.photos/200/300',
+    scores: [
+      1,
+      1,
+      3,
+      4,
+      5,
+      2,
+      1,
+      4,
+      4,
+      5
+    ]
+  },
+  {
+    name: 'Lizzy',
+    image: 'https://picsum.photos/200/300',
+    scores: [
+      1,
+      2,
+      3,
+      2,
+      5,
+      5,
+      4,
+      4,
+      4,
+      2
+    ]
+  },
+  {
+    name: 'Becca',
+    image: 'https://picsum.photos/200/300',
+    scores: [
+      1,
+      1,
+      5,
+      5,
+      1,
+      4,
+      2,
+      2,
+      3,
+      1
+    ]
+  },
+  {
+    name: 'Aaron',
+    image: 'https://picsum.photos/200/300',
+    scores: [
+      5,
+      5,
+      3,
+      1,
+      2,
+      2,
+      4,
+      3,
+      3,
+      3
+    ]
+  },
 ]
